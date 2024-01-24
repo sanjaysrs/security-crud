e0f5b14801f9e221e0036c4493144c010d8f1c8d
Sanjay <140075012+sanjaysrs@users.noreply.github.com>
2024-01-24 13:10:18 +0530

    list users in admin panel

--- src/main/java/com/srsanjay/twenty/controller/AdminController.java
+++ src/main/java/com/srsanjay/twenty/controller/AdminController.java
@@ -1,13 +1,24 @@
 package com.srsanjay.twenty.controller;
 
+import com.srsanjay.twenty.model.User;
+import com.srsanjay.twenty.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AdminController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/admin")
-    public String adminPanel() {
+    public String adminPanel(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
         return "admin";
     }
 
--- src/main/java/com/srsanjay/twenty/service/UserService.java
+++ src/main/java/com/srsanjay/twenty/service/UserService.java
@@ -30,4 +30,8 @@ public class UserService {
         return userRepository.findByUsername(username);
     }
 
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
 }
